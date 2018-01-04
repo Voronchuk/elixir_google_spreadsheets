d124b63169c0da773fdb399b3cb87d01f37cedea
Vyacheslav Voronchuk <voronchuk@gmail.com>
2018-01-04 15:36:57 +0100

    Added range tests into separate file, so reduce quota usage in test setup

--- test/elixir_google_spreadsheets/spreadsheet_test.exs
+++ test/elixir_google_spreadsheets/spreadsheet_test.exs
@@ -85,42 +85,4 @@ defmodule GSS.SpreadsheetTest do
         {:ok, result} = GSS.Spreadsheet.read_rows(pid, 2, 3, column_to: 6)
         assert result == [@test_row1 ++ [""], @test_row2]
     end
-
-    describe "range/4 with one row" do
-        test "generates range for length 14" do
-            assert GSS.Spreadsheet.range(1, 1, 1, 14) == "A1:N1"
-        end
-
-        test "generates range for length 26" do
-            assert GSS.Spreadsheet.range(1, 1, 1, 26) == "A1:Z1"
-        end
-
-        test "generates range for length 27" do
-            assert GSS.Spreadsheet.range(1, 1, 1, 27) == "A1:AA1"
-        end
-
-        test "generates range for length 52" do
-            assert GSS.Spreadsheet.range(1, 1, 1, 52) == "A1:AZ1"
-        end
-
-        test "generates range for length 53" do
-            assert GSS.Spreadsheet.range(1, 1, 1, 53) == "A1:BA1"
-        end
-
-        test "genearates range for length 254" do
-            assert GSS.Spreadsheet.range(1, 1, 1, 254) == "A1:IT1"
-        end
-
-        test "generates range for length 255" do
-            assert GSS.Spreadsheet.range(1, 1, 1, 255) == "A1:IU1"
-        end
-
-        test "generates range for length 702" do
-            assert GSS.Spreadsheet.range(1, 1, 1, 702) == "A1:ZZ1"
-        end
-
-        test "generates range for length 703" do
-            assert GSS.Spreadsheet.range(1, 1, 1, 703) == "A1:AAA1"
-        end
-    end
 end
